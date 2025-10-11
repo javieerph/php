d1d0569fab983a3713a6de53eed7fa59b274effe
Javier <javieer55ph@gmail.com>
2025-10-11 20:24:25 +0100

    v1

---
+++ ignition.php
@@ -0,0 +1,16 @@
+<?php
+//los numeros del 1 al 10
+
+$i = 10;
+
+do
+{
+	echo $i . " ";
+	$i--;
+}
+while ($i > 0);
+echo "<br><br>";
+echo "!!!!Ignition!!!!" . "<br><br>";
+echo '<img src="cohete.png" alt="Foto de ejemplo">';
+
+?>
