c579af93ade0b6844a1ce198d9e17cd203347483
Javier <javieer55ph@gmail.com>
2025-10-11 19:01:20 +0100

    v1

---
+++ tablas.php
@@ -0,0 +1,13 @@
+<?php
+// tablas de multiplicar
+
+$tabla = 5;
+
+echo "La tabla del " . $tabla . "<br><br>";
+
+for ($i = 1; $i <= 9; $i++)
+{
+	echo $tabla . " x " . $i . " = " . $tabla * $i . "<br>";
+}
+
+?>
