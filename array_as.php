6253d59ed46d75833bf53c91b256eeaf4c49b1d1
Javier <javieer55ph@gmail.com>
2025-10-13 21:48:58 +0100

    v01

---
+++ array_as.php
@@ -0,0 +1,12 @@
+<?php
+$names = array("Sandra" => "19/05/1986", "Maria" => "20/11/2002", "Jose" => "20/10/1994", "Javier" => "20/06/204", "Julieta" => "15/01/2014", "Matias" => "20/10/2025");
+
+	
+echo "La lista de los participantes es: " . "<br><br>";
+	
+echo "*" . $names["Sandra"] . "<br>";
+echo "*" . $names["Julieta"] . "<br>";
+echo "*" . $names["Matias"] . "<br>";
+echo "*" . $names["Javier"] . "<br>";
+
+?>
